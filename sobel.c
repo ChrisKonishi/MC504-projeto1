7309f1020f5ad0b31b187501ba960cfdb163969f
Gabriel de Freitas Garcia <gabrielfreitasgarcia@globo.com>
2021-04-03 18:25:25 -0300

    Some auxiliary changes

--- sobel.c
+++ sobel.c
@@ -17,9 +17,40 @@ image* sobel(image* img){
                     Gy[3][3] = {{1,2,1},
                                 {0,0,0},
                                 {-1,-2,-1}};
-    sobelArg* arg = malloc(sizeof (sobelArg));
+    image* nImg;
+    sobelArg* arg = (sobelArg*) malloc(sizeof(sobelArg));
     arg->Gx = (int**) Gx;
     arg->Gy = (int**) Gy;
-    arg->img = img;
+    nImg = pad(img,1);
+    arg->img = nImg;
 
+}
+
+int** getNeighbor(int x, int y, image* img){
+    int** m = createMatrix(3,3);
+    if(x < 1 || x > img->h-2 || y < 1 || y < img->w - 2){
+        printf("error\n");
+        return NULL;
+    }else {
+        for (int i = 0, v = -1; i < 3, v < 2; i++, v++){
+            for (int j = 0, h = -1; j < 3, h < 2; j++, h++){
+                m[i][j] = read_pixel(img,x+h,y+v);
+            }
+        }
+        return m;
+    }
+}
+
+int** createMatrix(int n,int m){
+    int** matrix = (int**) malloc(m*sizeof(int*));
+    for(int i = 0; i < m; i++){
+        matrix[i] = (int*) malloc(n*sizeof(int));
+    }
+    return matrix;
+}
+void freeMatrix(int** matrix, int n, int m){
+    for(int i = 0; i < m; i++){
+        free(matrix[i]);
+    }
+    free(matrix);
 }
