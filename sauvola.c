1001a1df8bcb099b1b1fd132c2a13f9a0bab88a1
unknown <magodocona@gmail.com>
2021-04-06 11:44:02 -0300

    fixed line indexes

--- sauvola.c
+++ sauvola.c
@@ -35,15 +35,16 @@ image *apply_sauvola(image *img, float R, float k, int n);
 
 void *sauvola(void *args) {
     sauvola_args *arg = (sauvola_args*) args;
-    int sum, squared_sum, val;
+    int sum, squared_sum;
+    unsigned char val, pixel;
     int n = arg->n;
     double average, sqr_avg, std_dev;
 
     image *img = arg->img;
     image *nova_img = arg->nova_img;
 
-    int row, col, i, j;
-    int pixel;
+    int row, col, i, j, p_row, p_col;
+    float limiar;
 
     for (row = arg->start; row <= arg->end; row++) {
         for (col = 0; col < img->w; col++) {
@@ -51,17 +52,24 @@ void *sauvola(void *args) {
             squared_sum = 0;
             for (i = -n/2; i <= n/2; i++) {
                 for (j = -n/2; j <= n/2; j++) {
-                    val = img->img[(row + i + ((int) n/2))* img->h + col + j + ((int) n/2)];
-                    sum += val;
-                    squared_sum += pow(val, 2);
+                    p_row = row + i + (int) n/2;
+                    p_col = col + j + (int) n/2;
+                    val = read_pixel(img, p_row, p_col);
+                    sum += (int) val;
+                    squared_sum += pow( (int) val, 2);
                 }
             }
             
             average = sum/(n * n);
             sqr_avg = squared_sum/(n * n);
             std_dev = sqrt(sqr_avg - pow(average, 2)); 
-            pixel = average * (1 + arg->k * (std_dev/arg->R - 1));
-            // printf("Pixel: %d\n", pixel);
+            limiar = average * (1 + arg->k * (std_dev/arg->R - 1));
+            
+            pixel = read_pixel(img, row, col);
+            if (pixel > limiar) 
+                pixel = 0;
+            else
+                pixel = 255;
             write_pixel(nova_img, row, col, (unsigned char) pixel);
         } 
     }
@@ -70,35 +78,42 @@ void *sauvola(void *args) {
 image *apply_sauvola(image *img, float R, float k, int n) {
     pthread_t thr[N_THR];
     sauvola_args args[N_THR];
-    int img_div;
+    int img_div, resto, start, end;
     image *nova_img, *pimg;
-
-    if (img->h % N_THR) 
-        img_div = (img->h/N_THR) + 1;
+    img_div = img->h / N_THR;
+    resto = img->h % N_THR;
+    printf("Resto: %d\n", resto);
     
-    else 
-        img_div = img->h/N_THR;
-    
-    pimg = new_img(img->h + (int) n/2, img->w + (int) n/2);
     pimg = pad(img, n/2);
     nova_img = new_img(img->h, img->w);
 
+    start = 0;
     for (int i = 0; i < N_THR; i++) {
         args[i].img = pimg;
         args[i].nova_img = nova_img;
         args[i].R = R;
         args[i].k = k;
         args[i].n = n;
-        args[i].start = i * img_div;
-        args[i].end = ((i + 1) * img_div) - 1;
+        args[i].start = start;
 
+        end = start + img_div - 1; 
+            
+        if (i < resto) 
+            end++;
+        
+        if (end == img->h) {
+            end = img->h;
+            args[i].end = img->h;
+        }
+        args[i].end = end;
+        printf("start, end: %d, %d\n", start, end);
+        
+        start = end + 1;
         pthread_create(&thr[i], NULL, sauvola, (void*) &args[i]);
     }
 
     for (int i=0; i<N_THR; i++)
-        pthread_join(thr[i], NULL); 
-
-    write_img("result.png", *nova_img);
+        pthread_join(thr[i], NULL);  
 
     return nova_img;
 }
@@ -114,8 +129,8 @@ int main(int argc, char* arg[]){
     read_img(arg[2], img);
 
     image* sauvola_img = apply_sauvola(img, 0.5, 0.05, 5);
-    printf("Return successfull\n");
-    
-    printf("Test\n");
+
+    write_img("result.png", *sauvola_img);
+
     return 0;
 }
