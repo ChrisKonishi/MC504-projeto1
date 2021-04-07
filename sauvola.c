cc04f4dd9b924928098f9175d4226b28e3f60db2
Chris Konishi <christian.konishi@gmail.com>
2021-04-07 20:48:30 -0300

    sauvola method

--- sauvola.c
+++ sauvola.c
@@ -1,37 +1,13 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <math.h>
+#include <stdio.h>
 
 #include "img_utils.h"
+#include "sauvola.h"
 
-// #define N_THR 8
+#define N_THR 8
 
-typedef struct {
-/* arguments for the sauvola function 
-    image *img: image to be filtered (needs to be padded)
-    image *new_img: result image
-    int R, k: sauvola arguments
-    int start, end: lines of the matrix to be modified
-*/
-    image *img, *nova_img;
-    int n, start, end;
-    float R, k;
-
-} sauvola_args;
-
-void *sauvola(void *args);
-/* function to be called from pthread_create
-    Arguments: 
-        sauvola_args *args: struct described above
-*/
-image *apply_sauvola(image *img, float R, float k, int n);
-/* function that creates the p-threads and distributes the work
-    Arguments:
-        image *img: image to be filtered
-        int R, k: sauvola arguments
-    Return:
-        new, filtered image
-*/
 
 void *sauvola(void *args) {
     sauvola_args *arg = (sauvola_args*) args;
@@ -44,18 +20,19 @@ void *sauvola(void *args) {
     image *nova_img = arg->nova_img;
     int row, col, i, j, p_row, p_col;
     float limiar;
+    int pad = n/2;
 
     for (row = arg->start; row <= arg->end; row++) {
-        for (col = 0; col < (img->w - n + 1); col++) {
+        for (col = 0; col < (nova_img->w); col++) {
             sum = 0;
             squared_sum = 0;
             for (i = -n/2; i <= n/2; i++) {
                 for (j = -n/2; j <= n/2; j++) {
-                    p_row = row + i + (int)n/2;
-                    p_col = col + j + (int)n/2;
-                    val = read_pixel(img, p_row, p_col);
-                    sum += ((int) val)/255.0;
-                    squared_sum += pow(((int) val)/255.0, 2);
+                    p_row = row + i + pad;
+                    p_col = col + j + pad;
+                    val = read_pixel(img, p_col, p_row);
+                    sum += val/255.0;
+                    squared_sum += pow(val/255.0, 2);
                 }
             }
 
@@ -64,16 +41,16 @@ void *sauvola(void *args) {
             std_dev = sqrt(sqr_avg - pow(average, 2)); 
             limiar = average * (1 + arg->k * (std_dev/arg->R - 1));
             limiar = limiar * 255;
-            pixel = (int) read_pixel(img, row + (int) n/2, col + (int) n/2);
+            pixel = (int) read_pixel(img, col + pad, row + pad);
             if (pixel > limiar) 
                 pixel = 0;
             else
                 pixel = 255;
-            write_pixel(nova_img, row, col, (unsigned char) pixel);
+            write_pixel(nova_img, col, row, (unsigned char) pixel);
         } 
     }
-    int a = 0;
- }
+    return NULL;
+}
 
 image *apply_sauvola(image *img, float R, float k, int n) {
     pthread_t thr[N_THR];
@@ -114,22 +91,7 @@ image *apply_sauvola(image *img, float R, float k, int n) {
     for (int i=0; i<N_THR; i++)
         pthread_join(thr[i], NULL);  
 
+    free_img(pimg);
+
     return nova_img;
 }
-
-int main(int argc, char* arg[]){
-    if (argc < 3){
-        printf("Argument error\n");
-        exit(1);
-    }
-    
-    image *img = malloc(sizeof(image));
-
-    read_img(arg[2], img);
-
-    image* sauvola_img = apply_sauvola(img, 0.5, 0.05, 15);
-
-    write_img("result.png", *sauvola_img);
-
-    return 0;
-} 
